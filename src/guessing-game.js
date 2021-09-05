4ae5ad851bebf6575cb3d87f343b901525ca06ab
Vladislava96 <vladislava96.96@mail.ru>
2021-09-05 16:00:39 +0300

    init: start guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.gameMin = min, // 1
+        this.gameMax = max // 10 // 5
     }
 
     guess() {
-
+        this.result = Math.round(this.gameMin + (this.gameMax - this.gameMin) / 2); // 5 // 1 + (5 - 1) / 2 = 3;
+        return this.result; // 5 // 3
     }
 
     lower() {
-
+        // (4)
+        // 1 --- 10
+        // 5     (5 > 4)
+        this.gameMax = this.result; // 5
     }
 
     greater() {
-
+        // (4)
+        // 1 --- 5
+        // 3     (3 < 4)
+        this.gameMin = this.result; // 3
     }
 }
 
